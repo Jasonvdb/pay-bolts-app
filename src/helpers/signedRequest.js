056f8f8a54881ab73f31b1b6c9cd20bfa06afdb4
Jason van den Berg <jayvdb1@gmail.com>
2018-06-16 20:35:27 +0200

    Signing requests with api key and secret

--- src/helpers/signedRequest.js
+++ src/helpers/signedRequest.js
@@ -1,6 +1,21 @@
 import axios from "axios";
+import crypto from "crypto";
 
 import { getCacheCallback } from "./localcache";
+
+const createApiSignature = (message, secret) =>
+	crypto
+		.createHmac("sha256", secret)
+		.update(message)
+		.digest("hex");
+
+const headers = (path, secret) => {
+	return {
+		"User-Agent": "Mozilla/4.0 (compatible; PayBoltsApp)",
+		"Content-type": "application/x-www-form-urlencoded",
+		apisign: createApiSignature(path, secret)
+	};
+};
 
 const executeRequest = ({
 	method,
@@ -11,13 +26,22 @@ const executeRequest = ({
 	onSuccess,
 	onError
 }) => {
-	//TODO sign the URL with the secret and place that hash in the header
+	let path = `/api/${method}?apiKey=${apiKey}`;
+
+	//Append params to the uri var and don't pass them to axios so we can sign the whole path
+	Object.keys(params).forEach(paramKey => {
+		path += `&${paramKey}=${params[paramKey]}`;
+	});
+
+	const formattedUrl = apiUrl.replace(/\/$/, "");
+	const uri = `${formattedUrl}${path}`;
+
 	axios
-		.get(`${apiUrl}api/${method}`, {
-			params: { ...params, apiKey }
+		.get(uri, {
+			headers: headers(path, secret)
 		})
 		.then(response => {
-			const { data } = response;
+			const data = response.data || {};
 			onSuccess(data);
 		})
 		.catch(errorResult => {
@@ -25,6 +49,7 @@ const executeRequest = ({
 			if (response.data && response.data.error && response.data.error.message) {
 				onError(response.data.error.message);
 			} else {
+				console.log(errorResult);
 				onError("An API error occured");
 			}
 		});
